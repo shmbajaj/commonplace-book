fda9d5d8ce0853827d8ae855ae71b94c1389c3bc
ShubhamBajaj <shmbajaj32@gmail.com>
2022-03-30 12:18:47 +0530

    reafctor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import {logoPath} from "./data";
 function App() {
   return (
     <div className="App">
+      <img src={logoPath} style={{width:"40px", height:"30px"}}/>
       <h1>I am note-taking-app</h1>
     </div>
   );
