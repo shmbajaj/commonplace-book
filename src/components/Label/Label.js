ef20e059acb8ecb05b89e4c9346d5ffa7a9ed606
ShubhamBajaj <shmbajaj32@gmail.com>
2022-04-05 02:34:14 +0530

    feat: functionality added

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -1,6 +1,7 @@
 import styles from "./Label.styles.module.css";
 
-function Label({ labelText }) {
+function Label({label}) {
+  const { labelText }= label;
   return (
     <p className={`${styles.label}`}>
       {labelText}
--- src/components/LabelList/LabelList.js
+++ src/components/LabelList/LabelList.js
@@ -1,16 +1,25 @@
 import { Label } from "../Label/Label";
 import styles from "./LabelList.styles.module.css";
+import { useNotes } from "context";
 
 function LabelList({ list, active }) {
+  const {
+    notesState: { activeNote },
+  } = useNotes();
   return (
     <div className={styles.listWrapper}>
-      <span className={`material-icons ${styles.listIcon} ${active? styles.listIconActive : ""} `}>
-        label
-      </span>
+      {list && (
+        <span
+          className={`material-icons ${styles.listIcon} ${
+            active ? styles.listIconActive : ""
+          } `}
+        >
+          label
+        </span>
+      )}
       <ul className={styles.labelsList}>
-        {list.map(({ labelText }, index) => (
-          <Label key={index} labelText={labelText}/>
-        ))}
+        {list &&
+          list.map((label, index) => <Label key={index} label={label} />)}
       </ul>
     </div>
   );
