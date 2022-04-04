dca3e0a5d8d768300ce4c1e56256cce6866cf299
ShubhamBajaj <shmbajaj32@gmail.com>
2022-04-05 02:32:16 +0530

    feat: get notes meta info

--- src/components/NoteList/Note/Note.js
+++ src/components/NoteList/Note/Note.js
@@ -1,12 +1,56 @@
 import styles from "./Note.styles.module.css";
 import { LabelList } from "../../LabelList/LabelList";
+import { useNotes, useFeatureBar } from "context";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function Note({note}) {
-  const { title, labelsList, active } = note;
+function Note({ note }) {
+  const { notesState, notesDispatch, updateNote, replicaNote, archiveNote,setReplicaNote } =
+    useNotes();
+  const { state } = useFeatureBar();
+
+  async function updateNotesIndependtly(note) {
+    const type = state.activeFeature === "notes" ? "notes" : "archives";
+    const noteToBeUpdated = { ...notesState.activeNote, ...replicaNote };
+
+    if (type === "notes") {
+      const  notes  = await updateNote(noteToBeUpdated);
+       notesDispatch({
+        type: "NOTES",
+        payload: notes,
+      });
+    } else {
+      const { notes, archives } = await archiveNote(noteToBeUpdated);
+       notesDispatch({
+        type: "NOTES",
+        payload: archives,
+      });
+    }
+
+    setReplicaNote((prev) => ({ ...prev, ...note }));
+    notesDispatch({ type: "ACTIVE_NOTE", payload: note });
+  }
+
+  function onNoteClickHandler(note) {
+    if (notesState.activeNote) {
+      updateNotesIndependtly(note);
+    }
+    setReplicaNote((prev) => ({ ...prev, ...note }));
+    notesDispatch({ type: "ACTIVE_NOTE", payload: note });
+  }
+
   return (
-    <div className={`${styles.metaNote} ${active ? styles.active : ""}`}>
-      <p className={styles.title}>{title}</p>
-      <LabelList list={labelsList} active={active} />
+    <div
+      className={`${styles.metaNote} ${
+        note._id === notesState.activeNote._id ? styles.active : ""
+      }`}
+      onClick={() => onNoteClickHandler(note)}
+    >
+      <p className={styles.title}>{note.title}</p>
+      <LabelList
+        list={note.labels}
+        active={note._id === notesState.activeNote._id}
+      />
     </div>
   );
 }
--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -3,12 +3,8 @@ import { Note } from "./Note/Note";
 function NoteList({ list }) {
   return (
     <ul>
-      {list.map((note, index) => (
-        <Note
-          note = {note}
-          key={index}
-        />
-      ))}
+      {list?.length !== 0 &&
+        list.map((note) => <Note note={note} key={note._id} />)}
     </ul>
   );
 }
